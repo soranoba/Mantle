22374bba6ffc2ea539db610934c128d08acc2a88
soranoba <soranoba@gmail.com>
2017-10-06 01:10:59 +0900

    supress duplicate error

--- Mantle/a.h
+++ Mantle/a.h
@@ -1,8 +1,9 @@
+
 @import Foundation;
 
 // Unlike import, include does not have the same file load guard, so you can reproduce errors in a pseudo manner.
-#include "b.h"
-#include "c.h"
+#include "b.h" // include b -> include c
+#include "c.h" // include c :: duplicate load!!
 
 @interface NSObject (A)
 @end
--- Mantle/c.h
+++ Mantle/c.h
@@ -1,5 +1,10 @@
+#ifndef C_H
+#define C_H
+
 @import Foundation;
 #import "MTLModel.h"
 
 @interface NSObject (C)
 @end
+
+#endif
